e87708f5861ba62eb1287bb30463d5a9d4d642c9
YITU\shenjun <895017142@qq.com>
2019-07-19 15:48:44 +0800

    06

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,33 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @Bean
+    public Runnable createRunnable(){
+        return ()->{System.out.println("spring boot is run");};
+    }
 
+    public static void main(String[] args) {
+        //静态方法
+        ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+
+        //这种写法也行
+        //SpringApplication app = new SpringApplication(DemoApplication.class);
+        //ConfigurableApplicationContext context = app.run(args);
+
+
+        context.getBean(Runnable.class).run();
+        System.out.println( context.getBean(User.class));
+
+        System.out.println(context.getBean(List.class));
+    }
 }

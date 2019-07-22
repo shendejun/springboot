b93035841b89396acf753494c5a3f0683ddb6f60
YITU\shenjun <895017142@qq.com>
2019-07-22 16:41:50 +0800

    通过启动参数来控制生效的 profile 编码的方式制定生效的profile

--- demo_07/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo_07/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,14 +5,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.context.ConfigurableApplicationContext;
 
+/**
+ * 通过启动参数来控制生效的 profile
+ * --spring.profiles.active=test
+ * 多个
+ * --spring.profiles.active=test,dev
+ */
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+//        ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+
+
+        SpringApplication app = new SpringApplication(DemoApplication.class);
+        //编码的方式制定生效的profile
+        app.setAdditionalProfiles("test");
+        ConfigurableApplicationContext context = app.run(args);
+
         System.out.println(context.getEnvironment().getProperty("jdbc.username"));
         System.out.println(context.getEnvironment().getProperty("jdbc.password"));
         System.out.println(context.getEnvironment().getProperty("springboot.name"));
+
+        System.out.println(context.getEnvironment().getProperty("config"));
         System.out.println(context.getBean(TomcatProperties.class));
         context.close();
 

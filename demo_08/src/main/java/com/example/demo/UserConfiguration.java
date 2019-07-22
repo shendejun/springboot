80a810b80fc3de895872c8a5d2605430966c6092
YITU\shenjun <895017142@qq.com>
2019-07-22 17:44:26 +0800

    ConditionalOnClass存在某个类的时候进行装配

--- demo_08/src/main/java/com/example/demo/UserConfiguration.java
+++ demo_08/src/main/java/com/example/demo/UserConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 
@@ -14,6 +15,13 @@ public class UserConfiguration {
     //ConditionalOnProperty 根据属性进行装配,matchIfMissing配置不存在的时候默认装配
     @ConditionalOnProperty(name = "runnable.enabled",havingValue = "true",matchIfMissing = true)
     public Runnable createRunnable(){
+        return ()->{};
+    }
+
+    @Bean
+    //ConditionalOnClass存在某个类的时候进行装配
+    @ConditionalOnClass(name = "com.google.gson.Gson")
+    public Runnable createGsonRunnable(){
         return ()->{};
     }
 }

922505eaaa8f8c0546358cee52d69741fdaa8113
YITU\shenjun <895017142@qq.com>
2019-07-22 16:55:10 +0800

    可以整个类里注解启用哪个profile

--- demo_07/demo/src/main/java/com/example/demo/MyConfig.java
+++ demo_07/demo/src/main/java/com/example/demo/MyConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Profile;
  *
  */
 @SpringBootConfiguration
+@Profile("dev")
 public class MyConfig {
     @Bean
     public Runnable createRunnable(){
@@ -17,14 +18,14 @@ public class MyConfig {
 
     @Bean
     //这个注解说的是，只有test Profile激活之后才装配createRunnable2
-    @Profile("test")
+//    @Profile("test")
     public Runnable createRunnable2(){
         System.out.println("===========2===========");
         return ()->{};
     }
     @Bean
     //这个注解说的是，只有dev Profile激活之后才装配createRunnable2
-    @Profile("dev")
+//    @Profile("dev")
     public Runnable createRunnable3(){
         System.out.println("===========3===========");
         return ()->{};

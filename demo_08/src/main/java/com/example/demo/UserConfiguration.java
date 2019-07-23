c35b8b704176d6d23c67ebec0080f2c979d49ccb
YITU\shenjun <895017142@qq.com>
2019-07-23 10:25:35 +0800

    ConditionalOnBean根据容器中存在某个bean进行装配 name小写？ ConditionalOnMissingBean 不存在某个bean进行装配

--- demo_08/src/main/java/com/example/demo/UserConfiguration.java
+++ demo_08/src/main/java/com/example/demo/UserConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -18,10 +19,28 @@ public class UserConfiguration {
         return ()->{};
     }
 
+    /**
+     * ConditionalOnClass存在某个类的时候进行装配
+     * ConditionalOnMissingClass 不存在
+     * @return
+     */
     @Bean
-    //ConditionalOnClass存在某个类的时候进行装配
     @ConditionalOnClass(name = "com.google.gson.Gson")
     public Runnable createGsonRunnable(){
+        System.out.println("----------------------com.google.gson.Gson----------------------");
         return ()->{};
     }
+
+    /**
+     * ConditionalOnBean根据容器中存在某个bean进行装配 name小写？
+     * ConditionalOnMissingBean 不存在某个bean进行装配
+     * @return
+     */
+    @Bean
+    @ConditionalOnBean(name = "user")
+    public Runnable createBeanRunnable(){
+        System.out.println("----------------------user----------------------");
+        return ()->{};
+    }
+
 }

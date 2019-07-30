dacb57dd2689081f1155057daf89b13381dfc756
shendejun <895017142@qq.com>
2019-07-30 21:28:19 +0800

    需要监听那个事件，就写哪个Event

--- demo_11/demo/src/main/java/com/example/demo/MyEventHandle.java
+++ demo_11/demo/src/main/java/com/example/demo/MyEventHandle.java
@@ -8,8 +8,11 @@ public class MyEventHandle {
     /**
      * 参数任意
      * 所有该参数事件，或者其事件（子类）都可以接收到
+     *
      */
     @EventListener
+    //需要监听那个事件，就写哪个Event
+    //public void enent(MyApplicationEvent event) {
     public void enent(Object event) {
         System.out.println("接收到事件：" + event.getClass());
     }

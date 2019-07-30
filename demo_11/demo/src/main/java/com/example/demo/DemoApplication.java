508e9ea1622eab1c82e57d4a05811993fd24dfab
shendejun <895017142@qq.com>
2019-07-30 21:09:18 +0800

    发布事件

--- demo_11/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo_11/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication app = new SpringApplication(DemoApplication.class);
+        app.addListeners(new MyApplicationListener());
+        ConfigurableApplicationContext context = app.run(args);
+        //发布事件
+        context.publishEvent(new MyApplicationEvent(new Object()) );
+        context.close();
     }
 
 }
--- demo_11/demo/src/main/java/com/example/demo/MyApplicationListener.java
+++ demo_11/demo/src/main/java/com/example/demo/MyApplicationListener.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationListener;
  */
 public class MyApplicationListener implements ApplicationListener<MyApplicationEvent> {
     @Override
-    public void onApplicationEvent(MyApplicationEvent myApplicationEvent) {
-
+    public void onApplicationEvent(MyApplicationEvent event) {
+        System.out.println("接收到事件："+event.getClass());
     }
 }

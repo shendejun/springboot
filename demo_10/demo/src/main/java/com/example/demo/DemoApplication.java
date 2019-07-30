481c528072e4ca986d1c11b8b6a0fcd20dc7a10e
shendejun <895017142@qq.com>
2019-07-30 20:32:44 +0800

    添加不同项目bean注入

--- demo_10/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo_10/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+/**
+ * @EnableAutoConfiguration 作用：从class中搜索所有META-INF/spring.factories配置文件
+ * 然后，将其中org.springframework.boot.autoconfigure.EnableAutoConfiguration key对应的配置项加载到spring容器
+ * 需要在pom文件中添加包引用
+ *
+ * 
+ */
+@EnableAutoConfiguration
+@ComponentScan
 public class DemoApplication {
 
+    /**
+     * springboot入口，启动类
+     * @param args
+     */
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication app = new SpringApplication(DemoApplication.class);
+        ConfigurableApplicationContext context = app.run(args);
+        System.out.println(context.getBean(Runnable.class));
+        context.close();
     }
 
 }

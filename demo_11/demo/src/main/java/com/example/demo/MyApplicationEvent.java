31306676a699f899fc08b2ee7417e0c48ee55f3f
shendejun <895017142@qq.com>
2019-07-30 21:02:42 +0800

    定义事件

---
+++ demo_11/demo/src/main/java/com/example/demo/MyApplicationEvent.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.context.ApplicationEvent;
+
+/**
+ * 定义事件
+ */
+public class MyApplicationEvent extends ApplicationEvent {
+
+    private static final long serialVersionUID = 1L;
+
+    public MyApplicationEvent(Object source) {
+        super(source);
+    }
+}

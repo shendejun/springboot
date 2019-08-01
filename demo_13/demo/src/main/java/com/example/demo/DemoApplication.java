8feef845fc79a40667b71a9052307dd42ceccea6
shendejun <895017142@qq.com>
2019-08-01 22:40:04 +0800

    /**  * @SpringBootApplication 默认扫描的包的路径是当前包以及包下面的所有类，可以通过scanBasePackages 修改扫描包的路径  *  * 排查制定的类、配置类  * exclude 根据class来排查  * excludeName 根据class name来排查  *  * SpringApplication.setBannerMode(Banner.Mode.OFF);关闭输出banner  * 自定义banner方法  * 在classpath下放一个banner.txt的文件即可  * banner.location 配置项来制定文字在banner的文件路径  * banner.charset 配置项用来制定banner的编码，默认UTF-8  *  * springboot支持图片的banner，提供格式支持jpg,png,gif  * banner.inamge.location 配置项用来制定banner的文件路径  *  */

--- demo_13/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo_13/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+/**
+ * @SpringBootApplication 默认扫描的包的路径是当前包以及包下面的所有类，可以通过scanBasePackages 修改扫描包的路径
+ *
+ * 排查制定的类、配置类
+ * exclude 根据class来排查
+ * excludeName 根据class name来排查
+ *
+ * SpringApplication.setBannerMode(Banner.Mode.OFF);关闭输出banner
+ * 自定义banner方法
+ * 在classpath下放一个banner.txt的文件即可
+ * banner.location 配置项来制定文字在banner的文件路径
+ * banner.charset 配置项用来制定banner的编码，默认UTF-8
+ *
+ * springboot支持图片的banner，提供格式支持jpg,png,gif
+ * banner.inamge.location 配置项用来制定banner的文件路径
+ *
+ */
+@SpringBootApplication(scanBasePackages = "com.example.demo",excludeName = "")
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication app = new SpringApplication(DemoApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        ConfigurableApplicationContext context = app.run(args);
+        context.close();
     }
 
 }

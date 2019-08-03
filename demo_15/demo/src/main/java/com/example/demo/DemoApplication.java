f4f0370ff0103e46f7bc9eafb99f8dd3ac015232
zyuangui <2685376026@qq.com>
2019-08-03 17:52:52 +0800

    更换web 容器
    /**
     * 更换web容器
     * springboot web 默认使用tomcat做为web容器
     * 步骤：
     * 1、排查tomcat依赖
     * 2、加入jetty依赖
     */

--- demo_15/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo_15/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 更换web容器
+ * springboot web 默认使用tomcat做为web容器
+ * 步骤：
+ * 1、排查tomcat依赖
+ * 2、加入jetty依赖
+ */
 @SpringBootApplication
 public class DemoApplication {
 

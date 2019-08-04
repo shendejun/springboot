4137fb135f8637473d89a67818b242bd244a4c3c
zyuangui <2685376026@qq.com>
2019-08-04 09:11:31 +0800

    静态资源路径
    /**
     * springboot 如何出来静态资源
     * 1、src/main/webapp目录下，就可以直接访问
     * 2、默认的静态资源路径是：classpath:[/META-INF/resources/,/resources/,/static/,/public/]
     * 3、可以通过配置项spring.resources.staticLocations修改默认的配置
     */

--- demo_16/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo_16/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * springboot 如何出来静态资源
+ * 1、src/main/webapp目录下，就可以直接访问
+ * 2、默认的静态资源路径是：classpath:[/META-INF/resources/,/resources/,/static/,/public/]
+ * 3、可以通过配置项spring.resources.staticLocations修改默认的配置
+ */
 @SpringBootApplication
 public class DemoApplication {
 

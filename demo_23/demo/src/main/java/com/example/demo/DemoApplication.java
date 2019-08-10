9c4c0ac243759210dd53694ab850d9b02dcfc7fc
zyuangui <2685376026@qq.com>
2019-08-10 17:56:09 +0800

    引入依赖

--- demo_23/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo_23/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-        context.close();
+        
     }
 
 }

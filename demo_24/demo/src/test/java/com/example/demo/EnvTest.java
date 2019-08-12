4f6dd0077a53a04615be6d12b31fbc3b11c55f14
shendejun <895017142@qq.com>
2019-08-12 23:02:54 +0800

    /**      * 还可以使用这种方式，测试未通过      * https://blog.csdn.net/xybelieve1990/article/details/84783645      * 搜索EnvironmentTestUtils      */

--- demo_24/demo/src/test/java/com/example/demo/EnvTest.java
+++ demo_24/demo/src/test/java/com/example/demo/EnvTest.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,25 @@ import org.springframework.test.context.junit4.SpringRunner;
  * 测试环境下没有才会加载正式环境下的配置文件
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//也可以使用这种方式给测试环境添加一些配置项
+@SpringBootTest(properties = {"app.version=1.0.0"})
 public class EnvTest {
     @Autowired
     private Environment environment;
 
+    /**
+     * 还可以使用这种方式，测试未通过
+     * https://blog.csdn.net/xybelieve1990/article/details/84783645
+     * 搜索EnvironmentTestUtils
+     */
+//    @Before
+//    public void init(){
+//        EnvironmentTestUtils.addEnvironment(environment,"app.admin.user=zhangsan");
+//    }
+
     @Test
     public void testValue(){
-        Assert.assertEquals("springboot",environment.getProperty("app.name"));
+        Assert.assertEquals("springboottest",environment.getProperty("app.name"));
+        Assert.assertEquals("1.0.0",environment.getProperty("app.version"));
     }
 }

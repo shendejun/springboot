191962bd8c88a353c18e5496ad62f673fe0a6a34
shendejun <895017142@qq.com>
2019-08-17 10:07:48 +0800

    * 微服务  * 把原先的一个大的系统，拆分成小的系统  * 每个小系统分别开发，测试，维护

--- demo_25/mall-web/src/main/java/com/example/mall/web/mall/web/MallWebApplication.java
+++ demo_25/mall-web/src/main/java/com/example/mall/web/mall/web/MallWebApplication.java
@@ -13,6 +13,11 @@ import java.util.Map;
  * 调用方法
  * 服务提供的是什么服务，rest(http),web service,rpc
  * rest RestTemplate,httpClient
+ *
+ * 微服务
+ * 把原先的一个大的系统，拆分成小的系统
+ * 每个小系统分别开发，测试，维护
+ *
  */
 //@SpringBootApplication
 public class MallWebApplication {

bd8effd87151785c8ac5b902f63e09e2a27915bd
shendejun <895017142@qq.com>
2019-08-17 10:06:14 +0800

    * 调用方法  * 服务提供的是什么服务，rest(http),web service,rpc  * rest RestTemplate,httpClient

--- demo_25/mall-web/src/main/java/com/example/mall/web/mall/web/MallWebApplication.java
+++ demo_25/mall-web/src/main/java/com/example/mall/web/mall/web/MallWebApplication.java
@@ -1,13 +1,31 @@
 package com.example.mall.web.mall.web;
 
+import com.example.mall.web.mall.web.bean.Product;
+import com.example.mall.web.mall.web.bean.Response;
+import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+import java.util.Map;
+
+/**
+ * 调用方法
+ * 服务提供的是什么服务，rest(http),web service,rpc
+ * rest RestTemplate,httpClient
+ */
+//@SpringBootApplication
 public class MallWebApplication {
 
+    static String BASE_URL = "http://localhost:8082";
+
     public static void main(String[] args) {
-        SpringApplication.run(MallWebApplication.class, args);
+        RestTemplate restTemplate = new RestTemplate();
+        String body = restTemplate.getForObject(BASE_URL + "/soa/product/1", String.class);
+        System.out.println(body);
+        Map<String ,Object> map= new Gson().fromJson(body,Map.class);
+        Product product = new Gson().fromJson(map.get("data").toString(),Product.class);
+        System.out.println(product.getClass());
     }
 
 }

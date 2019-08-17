c4aa7411fbdcef90b5807eebd18ec7560ed47425
shendejun <895017142@qq.com>
2019-08-17 09:50:55 +0800

    新建mall-web项目

---
+++ demo_25/mall-web/src/main/java/com/example/mall/web/mall/web/MallWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.mall.web.mall.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallWebApplication.class, args);
+    }
+
+}
